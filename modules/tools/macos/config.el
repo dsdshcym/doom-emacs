2e19d215e1624ef7fc285e58c16eb43673abcdc1
Yiming Chen <dsdshcym@gmail.com>
2018-01-10 18:48:15 +0800

    tools/macos: add dash-at-point

---
+++ modules/tools/macos/config.el
@@ -0,0 +1,4 @@
+;;; tools/macos/config.el -*- lexical-binding: t; -*-
+
+(def-package! dash-at-point
+  :commands (dash-at-point dash-at-point-with-docset))
---
+++ modules/tools/macos/packages.el
@@ -0,0 +1,4 @@
+;; -*- no-byte-compile: t; -*-
+;;; tools/macos/packages.el
+
+(package! dash-at-point)

c916ee8bd1d170c71f0961771ca93387b4c231a1
Yiming Chen <dsdshcym@gmail.com>
2018-01-15 16:44:26 +0800

    lang/elixir: add popup settings for alchemist buffers

--- modules/lang/elixir/config.el
+++ modules/lang/elixir/config.el
@@ -29,6 +29,10 @@
   :after elixir-mode
   :hook (elixir-mode . alchemist-mode)
   :config
+  (set! :popup "^\\*alchemist elixir"
+     '((size . 0.35)) '((transient . 0) (select . ignore)))
+  (set! :popup "^\\*alchemist test"
+     '((size . 0.35)) '((transient . 0) (select . ignore)))
   (set! :repl 'elixir-mode #'alchemist-iex-run)
   (set! :lookup 'elixir-mode
     :definition #'alchemist-goto-definition-at-point

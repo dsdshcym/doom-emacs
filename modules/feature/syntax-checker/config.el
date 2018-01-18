cbeb91eea21ffd20c8e4a5c79cbc6b762ff9166e
Yiming Chen <dsdshcym@gmail.com>
2018-01-18 10:23:53 +0800

    feature/syntax-checker: run flycheck when idle-change instead of escape
    - many operations are done in normal/visual mode instead of insert mode

--- modules/feature/syntax-checker/config.el
+++ modules/feature/syntax-checker/config.el
@@ -10,18 +10,6 @@
   ;; Emacs feels snappier without checks on newline
   (setq flycheck-check-syntax-automatically '(save idle-change mode-enabled))
 
-  (after! evil
-    (defun +syntax-checkers|flycheck-buffer ()
-      "Flycheck buffer on ESC in normal mode."
-      (when flycheck-mode
-        (ignore-errors (flycheck-buffer))
-        nil))
-    (add-hook 'doom-escape-hook #'+syntax-checkers|flycheck-buffer t)
-
-    ;; With the option of flychecking the buffer on escape, so we don't need
-    ;; auto-flychecking on idle-change:
-    (delq 'idle-change flycheck-check-syntax-automatically))
-
   ;; NOTE Adjust these bitmaps if you change `doom-fringe-size'
   ;; because git-gutter is in the left fringe
   (setq flycheck-indication-mode 'right-fringe)

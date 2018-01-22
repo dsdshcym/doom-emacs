6eef2aa130e02dc2c7129b8cff5bf3ae67a0f248
Yiming Chen <dsdshcym@gmail.com>
2018-01-22 15:53:15 +0800

    tools/macos: extract emacs-mac-os specific settings to +emacs-mac-port.el

---
+++ modules/tools/macos/+emacs-mac-port.el
@@ -0,0 +1,24 @@
+;;; tools/macos/+emacs-mac-port.el -*- lexical-binding: t; -*-
+
+(setq mac-pass-command-to-system nil)
+(mac-auto-operator-composition-mode t)
+
+(def-package! mac-win
+  :config
+  (setq mac-frame-tabbing nil)
+
+  (defun +mac-new-tab ()
+    "Create a new tab on mac"
+    (interactive)
+    (let ((mac-frame-tabbing t))
+      (new-frame)))
+
+  (defun +mac-kill-tab ()
+    "Kill a tab (Just an alias for delete-frame since a tab on
+mac is actually just a frame)"
+    (interactive)
+    (delete-frame))
+
+  (map!
+   :n "gt" 'mac-next-tab
+   :n "gT" 'mac-previous-tab))
--- modules/tools/macos/config.el
+++ modules/tools/macos/config.el
@@ -1,28 +1,7 @@
 ;;; tools/macos/config.el -*- lexical-binding: t; -*-
 
 (when (eq window-system 'mac)
-  (setq mac-pass-command-to-system nil)
-  (mac-auto-operator-composition-mode t)
-
-  (def-package! mac-win
-    :config
-    (setq mac-frame-tabbing nil)
-
-    (defun +mac-new-tab ()
-      "Create a new tab on mac"
-      (interactive)
-      (let ((mac-frame-tabbing t))
-        (new-frame)))
-
-    (defun +mac-kill-tab ()
-      "Kill a tab (Just an alias for delete-frame since a tab on
-mac is actually just a frame)"
-      (interactive)
-      (delete-frame))
-
-    (map!
-     :n "gt" 'mac-next-tab
-     :n "gT" 'mac-previous-tab)))
+  (load! +emacs-mac-port))
 
 (def-package! dash-at-point
   :commands (dash-at-point dash-at-point-with-docset))

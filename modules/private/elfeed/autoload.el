3d2918f5284af432ee1d5fd400db396209d9a3dc
Yiming Chen <dsdshcym@gmail.com>
2018-01-23 22:04:48 +0800

    private/elfeed: reformat autoload.el
    - wrong indentation

--- modules/private/elfeed/autoload.el
+++ modules/private/elfeed/autoload.el
@@ -2,12 +2,11 @@
 
 ;;;###autoload
 (defun +elfeed/org-elfeed-entry-store-link ()
-    (interactive)
-    (when (and (boundp 'elfeed-show-entry) elfeed-show-entry)
-      (let* ((link (elfeed-entry-link elfeed-show-entry))
-             (title (elfeed-entry-title elfeed-show-entry)))
-        (message title)
-        (org-store-link-props
-         :link link
-         :description title)
-        )))
+  (interactive)
+  (when (and (boundp 'elfeed-show-entry) elfeed-show-entry)
+    (let* ((link (elfeed-entry-link elfeed-show-entry))
+           (title (elfeed-entry-title elfeed-show-entry)))
+      (message title)
+      (org-store-link-props
+       :link link
+       :description title))))

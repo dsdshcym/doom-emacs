8e9f939deef7c55ad0c220ed2b3ff78940720ec4
Yiming Chen <dsdshcym@gmail.com>
2018-01-25 10:27:02 +0800

    chrome: set atomic-chrome-url-major-mode-alist for gitlab/github to gfm-mode

--- modules/private/chrome/config.el
+++ modules/private/chrome/config.el
@@ -4,5 +4,8 @@
   :hook (doom-post-init . atomic-chrome-start-server)
   :config
   (setq atomic-chrome-default-major-mode 'org-mode)
+  (setq atomic-chrome-url-major-mode-alist
+        '(("github" . gfm-mode)
+          ("gitlab" . gfm-mode)))
   (setq atomic-chrome-buffer-open-style 'frame)
   (add-hook 'atomic-chrome-edit-done-hook 'delete-frame))
